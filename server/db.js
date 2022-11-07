3721857d6f80c02f2dabd29fe5368a0f097d5d38
Marcus Vinicius De Siqueira Morais <v.marcus@academico.ifrn.edu.br>
2022-11-07 14:39:46 -0300

    configurando com server online

--- server/db.js
+++ server/db.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user: "postgres",
-    password: "abc123",
-    host: "localhost",
+    user: "ldwamxjx",
+    password: "zmaCsUrQ8GHrquyROK8T01gMXxjMcoz5",
+    host: "babar.db.elephantsql.com",
     port: 5432,
-    database: "filazero"
+    database: "ldwamxjx"
 });
 
 
